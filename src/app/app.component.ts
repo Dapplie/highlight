85b3561247202a0f888c34dd75078e190df93942
unknown <pipodany5@gmail.com>
2024-05-03 16:46:35 +0300

    main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,13 +17,14 @@ export class AppComponent {
   title = 'highlightproj';
   searchText='';
   text=`loremLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`
+  text2=`This is the title`
   count =0;
   highlightedElements!: any;
 
   updateCount() {
     this.highlightedElements = this.el.nativeElement.querySelectorAll('.highlighted-text');
     this.count = this.highlightedElements.length;
-    console.log('lol'+this.highlightedElements.length);
+    //console.log('lol'+this.highlightedElements.length);
   }
 }
 
